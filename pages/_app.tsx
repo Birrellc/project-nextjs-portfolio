542f8615871eb8d26420c63744a03cfccfdd1503
birrellc <vaiezmedia@gmail.com>
2023-03-31 17:52:19 +0100

    Feat: added Bebas Neue font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Bebas_Neue } from 'next/font/google';
+
+// https://nextjs.org/docs/basic-features/font-optimization
+const bebas = Bebas_Neue({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-bebas',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${bebas.variable} font-bebas`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
